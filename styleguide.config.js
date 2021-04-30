aa559827245bb7cd97cf8a72bb7eedbda0e71ff0
Valyukhov <alexzed@bk.ru>
2021-04-30 08:11:54 +0300

    Some clean

--- styleguide.config.js
+++ styleguide.config.js
@@ -7,83 +7,37 @@ let sections = [
 		 name: 'README',
 		 content: 'README.md',
 	},
-	// {
-	// 	name: 'GL Book Package Checker Demo',
-	// 	components: () => {
-	// 		const componentNames = [
-	// 			'gl-book-package-check',
-	// 		];
-	// 		return componentNames.map(componentName => {
-	// 			const filename = upperFirst(camelCase(componentName));
-	// 			return path.resolve(__dirname, `src/demos/${componentName}`, `${filename}.js`)
-	// 		});
-	// 	}
-	// },
-	
 	{
-		// The difficulty with displaying the various functions this way
-		//	is that they all appear and run on a SINGLE web-page.
 		name: 'BookList collection',
-		// content: 'src/core/README.md',
+		
 		sections: [
 			{
 				name: 'Book',
 				content: 'src/components/Book/Book.md',
-				// description: ''
 			},
 			{
 				name: 'BookList',
 				content: 'src/components/BookList/BookList.md',
-				// description: ''
 			},			
 		]
 	},
 	{
-		// The difficulty with displaying core functions this way
-		//	is that they all appear and run on a SINGLE web-page.
+		
 		name: 'ChapterList collection',
-		// content: 'src/core/README.md',
+		
 		sections: [
 			{
 				name: 'Chapter',
 				content: 'src/components/Chapter/Chapter.md',
-				// description: ''
 			},
 			{
 				name: 'ChapterList',
 				content: 'src/components/ChapterList/ChapterList.md',
-				// description: ''
 			}
 			
 		]
 	},
-	
 ];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = {
   components: "src/components/**/[A-Z]*.js",
