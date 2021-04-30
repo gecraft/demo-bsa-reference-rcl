3629d307c07dea0d888aa4226a8533b09826ec66
Valyukhov <alexzed@bk.ru>
2021-04-30 07:51:25 +0300

    Begin?

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,6 +1,90 @@
 const path = require("path");
 const { name, version, url } = require("./package.json");
 
+
+let sections = [
+	{
+		// name: 'README',
+		// content: 'README.md',
+	},
+	// {
+	// 	name: 'GL Book Package Checker Demo',
+	// 	components: () => {
+	// 		const componentNames = [
+	// 			'gl-book-package-check',
+	// 		];
+	// 		return componentNames.map(componentName => {
+	// 			const filename = upperFirst(camelCase(componentName));
+	// 			return path.resolve(__dirname, `src/demos/${componentName}`, `${filename}.js`)
+	// 		});
+	// 	}
+	// },
+	
+	{
+		// The difficulty with displaying the various functions this way
+		//	is that they all appear and run on a SINGLE web-page.
+		name: 'Book collection',
+		// content: 'src/core/README.md',
+		sections: [
+			{
+				name: 'Book',
+				content: 'src/components/Book/Book.md',
+				// description: ''
+			},
+			{
+				name: 'BookList',
+				content: 'src/components/BookList/BookList.md',
+				// description: ''
+			},			
+		]
+	},
+	{
+		// The difficulty with displaying core functions this way
+		//	is that they all appear and run on a SINGLE web-page.
+		name: 'Chapter collection',
+		// content: 'src/core/README.md',
+		sections: [
+			{
+				name: 'Chapter',
+				content: 'src/components/Chapter/Chapter.md',
+				// description: ''
+			},
+			{
+				name: 'ChapterList',
+				content: 'src/components/ChapterList/ChapterList.md',
+				// description: ''
+			}
+			
+		]
+	},
+	
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = {
   components: "src/components/**/[A-Z]*.js",
   ribbon: {
@@ -10,6 +94,7 @@ module.exports = {
   title: `${name} v${version}`,
   moduleAliases: { "demo-bsa-reference-rcl": path.resolve(__dirname, "src") },
   skipComponentsWithoutExample: true,
+  sections,
   exampleMode: "expand",
   usageMode: "expand",
   getComponentPathLine(componentPath) {
